3e954dc6e95b96c57e981aed9bbb5113bddd7116
EmilyAbrahart <emilyabrahart@gmail.com>
2020-03-08 12:23:53 +0000

    contact page finished

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,21 +7,24 @@ const Contact = props => {
 		<ContactContainer counter={props.counter}>
 			<h2>CONTACT ME</h2>
 			<div>
-				<form action="https://formsubmit.co/emilyabrahart@gmail.com" method="POST">
-					<div>
-						<p>Name</p>
+				<StyledForm
+					action="https://formsubmit.co/emilyabrahart@gmail.com"
+					method="POST"
+				>
+					<FieldContainer>
+						<div className="label">NAME</div>
 						<input type="text" name="name" />
-					</div>
-					<div>
-						<p>Email</p>
+					</FieldContainer>
+					<FieldContainer>
+						<div className="label">EMAIL</div>
 						<input type="email" name="email" />
-					</div>
-					<div>
-						<p>Message</p>
+					</FieldContainer>
+					<FieldContainer>
+						<div className="label">MESSAGE</div>
 						<textarea name="message" />
-					</div>
-					<button type="submit">Submit</button>
-				</form>
+					</FieldContainer>
+					<FormButton type="submit">Submit</FormButton>
+				</StyledForm>
 			</div>
 		</ContactContainer>
 	);
@@ -38,4 +41,46 @@ const ContactContainer = styled.div`
 	background-color: ${color_subtle};
 	transition: all 0.5s ease-out;
 	color: ${color_dark};
+	h2 {
+		letter-spacing: 0.5rem;
+	}
+`;
+const StyledForm = styled.form`
+	width: 400px;
+`;
+
+const FieldContainer = styled.div`
+	width: 100%;
+	padding: 1rem 0;
+
+	.label {
+		letter-spacing: 0.5rem;
+	}
+
+	input {
+		width: 100%;
+		padding: 0.5rem 1rem;
+		font-size: 0.8rem;
+	}
+
+	textarea {
+		width: 100%;
+		height: 8rem;
+		padding: 0.5rem 1rem;
+		font-size: 0.8rem;
+	}
+`;
+
+const FormButton = styled.button`
+	background-color: ${color_dark};
+	color: ${color_subtle};
+	border: 2px solid ${color_dark};
+	padding: 0.5rem 1rem;
+	box-sizing: border-box;
+
+	&:hover {
+		cursor: pointer;
+		color: ${color_dark};
+		background-color: ${color_subtle};
+	}
 `;
