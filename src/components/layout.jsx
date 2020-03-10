a5da45b73a18d11d0d06c07cabb5df172170c7e4
EmilyAbrahart <emilyabrahart@gmail.com>
2020-03-10 16:46:49 +0000

    fix viewheight on mobile browsers

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import SiteContent from './siteContent';
 import styled from 'styled-components';
 import { FlexFunc, mobile } from '../styles';
+import useWindowDimensions from '../utils/useWindowDimensions';
 
 const Layout = () => {
+	const { height, width } = useWindowDimensions();
 	return (
-		<AppContainer>
+		<AppContainer height={height}>
 			<SiteContent />
 		</AppContainer>
 	);
@@ -16,12 +18,15 @@ export default Layout;
 const AppContainer = styled.div`
 	width: 100%;
 	height: 100vh;
+	height: ${props => props.height}px;
 	${FlexFunc('column', 'center', 'center')}
 	padding: 2rem;
 	overflow: hidden;
 
+
 	@media ${mobile} {
 		width: 100vw;
 		padding: 0;
+		overscroll-behavior: none;
 	}
 `;
